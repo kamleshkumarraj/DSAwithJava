aad7b679e4dc8dca9ba08898182850796759f936
Kamlesh Kumar <kushwahakamleshraj434@gmail.com>
2024-04-19 16:50:36 +0530

    Here we add a method for add node in first of the list

--- LinkedList.java
+++ LinkedList.java
@@ -3,14 +3,25 @@ public class LinkedList {
     Node tail = null;
     int count =0 ;
 
+    /*This method is used for add a node in last of the list. */
     public void addLast(int data) {
         Node temp = new Node(data);
-        count++;
+        this.count++;
         if(head == null && tail == null) this.head = temp;
         else this.tail.next = temp;
         this.tail = temp;
     }
 
+    /*This method is used for add a node in first of the list. */
+    public void addFirst(int data){
+        Node temp  = new Node(data);
+        this.count++;
+        if(this.head == null && this.tail == null) this.tail = temp;
+        else temp.next = this.head;
+        this.head = temp;
+    }
+
+    /*This method is used for printing the list on console. */
     public void printList() {
         Node temp = this.head;
         System.out.print("[ ");
@@ -21,6 +32,12 @@ public class LinkedList {
         System.out.println("]");
     }
     
+    /*This method is used for find the size of the list. */
+    public int size(){
+        return this.count;
+    }
+
+
 }
 
 class Main{
@@ -32,6 +49,12 @@ class Main{
         list.addLast(26);
         list.addLast(27);
         System.out.println("Our list is :");
+        list.printList();
+
+        list.addFirst(50);
+        list.addFirst(51);
+        list.addFirst(52);
+        System.out.println("After add node in first then our list is : ");
         list.printList();
     }
 }
